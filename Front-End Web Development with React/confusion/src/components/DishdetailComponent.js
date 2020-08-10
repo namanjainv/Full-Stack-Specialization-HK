6ed8db66a00200d9188c9080bd046fef25515c00
Naman Jain <namanjain@Namans-MacBook-Air.local>
2020-08-10 09:24:42 -0500

    Presentational and Container Components

--- Front-End Web Development with React/confusion/src/components/DishdetailComponent.js
+++ Front-End Web Development with React/confusion/src/components/DishdetailComponent.js
@@ -11,55 +11,36 @@ class Dishdetail extends Component {
     render() {
 
         const dish = this.props.selectedDish;
-
-        // Ref: https://stackoverflow.com/questions/27480262/get-current-date-in-dd-mon-yyy-format-in-javascript-jquery
-        Date.prototype.toShortFormat = function( ) {
-
-            let monthNames =["Jan","Feb","Mar","Apr",
-                              "May","Jun","Jul","Aug",
-                              "Sep", "Oct","Nov","Dec"];
-            
-            let day = this.getDate();
-            day = day > 9 ? day.toString() : "0" + day.toString()
-            
-            let monthIndex = this.getMonth();
-            let monthName = monthNames[monthIndex];
-            
-            let year = this.getFullYear();
-            
-            // return `${day}-${monthName}-${year}`;  
-            return `${monthName} ${day}, ${year}`;  
-        }
-
         if (dish != null) {
 
             const comments = dish.comments.map(( dishComment ) => {
                 return (
-                    <div key={dishComment.id}>
+                    <div key={ dishComment.id }>
                         <p> { dishComment.comment } </p>
-                        <p> -- { dishComment.author }, { ( new Date( dishComment.date ) ).toShortFormat() } </p>
+                        <p> -- { dishComment.author }, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(dishComment.date)))} </p>
                     </div>
                 );
             });
 
             return(
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12 col-md-5 m-1">
+                            <Card>
+                                <CardImg top src={dish.image} alt={dish.name} />
+                                <CardBody>
+                                <CardTitle>{dish.name}</CardTitle>
+                                <CardText>{dish.description}</CardText>
+                                </CardBody>
+                            </Card>
+                        </div>
 
-                <div className="row">
-                    <div  className="col-12 col-md-5 m-1">
-                        <Card>
-                            <CardImg top src={dish.image} alt={dish.name} />
-                            <CardBody>
-                            <CardTitle>{dish.name}</CardTitle>
-                            <CardText>{dish.description}</CardText>
-                            </CardBody>
-                        </Card>
+                        <div  className="col-12 col-md-5 m-1">
+                            <h3> Comments </h3>
+                            { comments }
+                        </div>
+
                     </div>
-
-                    <div  className="col-12 col-md-5 m-1">
-                        <h3> Comments </h3>
-                        { comments }
-                    </div>
-
                 </div>
             );
         }
--- Front-End Web Development with React/confusion/src/components/MenuComponent.js
+++ Front-End Web Development with React/confusion/src/components/MenuComponent.js
@@ -6,24 +6,12 @@ import DishDetail  from './DishdetailComponent'
 
 class Menu extends Component {
 
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            selectedDish: null
-        }
-    }
-
-    onDishSelect(dish) {
-        this.setState({ selectedDish: dish});
-    }
-
     render() {
         const menu = this.props.dishes.map((dish) => {
             return (
-              <div  className="col-12 col-md-5 m-1">
-                <Card key={dish.id}
-                  onClick={() => this.onDishSelect(dish)}>
+              <div className="col-12 col-md-5 m-1" key={dish.id}>
+                <Card
+                  onClick={() => this.props.onClick(dish.id)}>
                   <CardImg width="100%" src={dish.image} alt={dish.name} />
                   <CardImgOverlay>
                       <CardTitle>{dish.name}</CardTitle>
@@ -38,7 +26,6 @@ class Menu extends Component {
                 <div className="row">
                     {menu}
                 </div>
-                <DishDetail selectedDish={ this.state.selectedDish } />
             </div>
         );
     }
