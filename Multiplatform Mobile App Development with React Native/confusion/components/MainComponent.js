0046fef0c0068c91242d55a91c711654357c8843
Naman Jain <namanjain@Namans-MacBook-Air.local>
2020-08-17 21:34:23 -0500

    React Native Navigation 2.0

--- Multiplatform Mobile App Development with React Native/confusion/components/MainComponent.js
+++ Multiplatform Mobile App Development with React Native/confusion/components/MainComponent.js
@@ -3,9 +3,10 @@ import { View, Platform } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import Menu from './MenuComponent';
-import { DISHES } from '../shared/dishes';
+import Home from './HomeComponent';
 import Dishdetail from './DishdetailComponent';
 
 const options = {
@@ -17,7 +18,43 @@ const options = {
         color: "#fff"            
     }
 }
-const Stack = createStackNavigator();
+const MenuStack = createStackNavigator();
+
+const MenuNavigator = () => {
+    return(
+            <MenuStack.Navigator>
+                <MenuStack.Screen name="Menu" component={Menu} options={{ ...options, title: 'Menu' }} />
+                <MenuStack.Screen name="Dishdetail" component={Dishdetail} options={{ ...options, title: 'Dish Details' }} />
+            </MenuStack.Navigator>
+    )
+}
+
+const HomeStack = createStackNavigator();
+
+const HomeNavigator = () => {
+    return(
+            <HomeStack.Navigator>
+                <HomeStack.Screen name="Home" component={Home} options={{ ...options, title: 'Home' }} />
+            </HomeStack.Navigator>
+    )
+}
+
+const MainDrawer = createDrawerNavigator();
+
+const drawerOptions = {
+    backgroundColor: '#D1C4E9'
+}
+
+const MainNavigator = () => {
+    return (
+        <NavigationContainer>
+            <MainDrawer.Navigator style={ drawerOptions } >
+                <MainDrawer.Screen name="Home" component={ HomeNavigator } options={{ title: 'Home', drawerLabel: 'Home' }} />
+                <MainDrawer.Screen name="Menu" component={ MenuNavigator } options={{ title: 'Menu', drawerLabel: 'Menu' }}/>
+            </MainDrawer.Navigator>
+        </NavigationContainer>
+    )
+}
 
 class Main extends Component {
     constructor(props) {
@@ -25,16 +62,9 @@ class Main extends Component {
     }
 
     render() {
- 
         return (
             <View style={{flex:1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
-                
-                <NavigationContainer>
-                    <Stack.Navigator>
-                        <Stack.Screen name="Menu" component={Menu} options={{ ...options, title: 'Menu' }} />
-                        <Stack.Screen name="Dishdetail" component={Dishdetail} options={{ ...options, title: 'Dish Details' }} />
-                    </Stack.Navigator>
-                </NavigationContainer>
+                <MainNavigator />
             </View>
         );
     }
