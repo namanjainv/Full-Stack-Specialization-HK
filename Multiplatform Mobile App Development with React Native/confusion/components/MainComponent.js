d86cead1675695023287b72e6e09626b8046bd4f
Naman Jain <namanjain@Namans-MacBook-Air.local>
2020-08-17 21:03:26 -0500

    React Native Navigation 1.0

--- Multiplatform Mobile App Development with React Native/confusion/components/MainComponent.js
+++ Multiplatform Mobile App Development with React Native/confusion/components/MainComponent.js
@@ -1,28 +1,48 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Platform } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
 import Menu from './MenuComponent';
 import { DISHES } from '../shared/dishes';
 import Dishdetail from './DishdetailComponent';
 
+// const MenuNavigator = createStackNavigator({
+//     Menu: { screen: Menu },
+//     Dishdetail: { screen: Dishdetail }
+// },
+// {
+//     initialRouteName: 'Menu',
+//     navigationOptions: {
+//         headerStyle: {
+//             backgroundColor: "#512DA8"
+//         },
+//         headerTintColor: '#fff',
+//         headerTitleStyle: {
+//             color: "#fff"            
+//         }
+//     }
+// });
+
+const Stack = createStackNavigator();
+
 class Main extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            dishes: DISHES,
-            selectedDish: null
-        };
-    }
-
-    onDishSelect(dishId) {
-        this.setState({selectedDish: dishId})
     }
 
     render() {
  
         return (
-            <View>
-                <Menu dishes={this.state.dishes} onPress={(dishId) => this.onDishSelect(dishId)} />
-                <Dishdetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+            <View style={{flex:1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
+                
+                <NavigationContainer>
+                    <Stack.Navigator>
+                        <Stack.Screen name="Menu" component={Menu} options={{ title: 'Menu' }} />
+                        <Stack.Screen name="Dishdetail" component={Dishdetail} options={{ title: 'Dish Details' }} />
+                    </Stack.Navigator>
+                </NavigationContainer>
             </View>
         );
     }
